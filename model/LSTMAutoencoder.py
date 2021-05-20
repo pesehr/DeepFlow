de3caf33e69bcc7c2d873129168a06e89ca8e08a
Sepehr Sabour <sepehr.sabour@gmail.com>
2021-05-19 23:03:08 -0600

    new arch

---
+++ model/LSTMAutoencoder.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+from model.Decoder import Decoder
+from model.Encoder import Encoder
+from model.LSTM import LSTM
+
+
+class RecurrentAutoencoder(LSTM):
+    def __init__(self, n_features=1, embedding_dim=20):
+        super(RecurrentAutoencoder, self).__init__()
+        self.encoder = Encoder(n_features, embedding_dim)
+        self.decoder = Decoder(embedding_dim, n_features)
+        self.loss = nn.L1Loss(reduction='mean')
+
+    def forward(self, x):
+        seq_len = len(x[0])
+        latent = self.encoder(x, seq_len)
+        reconstructed = self.decoder(latent, seq_len)
+        return latent, self.loss(x[0], reconstructed)
